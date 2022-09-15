70e7010ed6fc71f3921dbce7bf96718f837785fc
Thibault <thibault.teisseire@etud.univ-jfc.fr>
2022-09-15 15:38:58 +0200

    Rajout 2 points sur affichages patient

--- src/Webpages/InfoPatient/PageInfoPatient.js
+++ src/Webpages/InfoPatient/PageInfoPatient.js
@@ -24,13 +24,13 @@ class PageInfoPatient extends React.Component {
             return(
                 <div>
                     <ul class="list-group">
-                        <li class="list-group-item">Nom     {this.state.patient.name[0].family}</li>
-                        <li class="list-group-item">Prenom  {this.state.patient.name[0].given[0]}</li>
-                        <li class="list-group-item">Genre    {this.state.patient.gender}</li>
-                        <li class="list-group-item">Date de Naissance   {this.state.patient.birthDate}</li>
-                        <li class="list-group-item">Tel     {this.state.patient.telecom[0].value}</li>
-                        <li class="list-group-item">Mail    {this.state.patient.telecom[1].value}</li>
-                        <li class="list-group-item">Adresse {this.state.patient.address[0].line[1] + ", " + this.state.patient.address[0].line[0] + " " + this.state.patient.address[0].city}</li>
+                        <li class="list-group-item">Nom :    {this.state.patient.name[0].family}</li>
+                        <li class="list-group-item">Prenom :  {this.state.patient.name[0].given[0]}</li>
+                        <li class="list-group-item">Genre :    {this.state.patient.gender}</li>
+                        <li class="list-group-item">Date de Naissance :   {this.state.patient.birthDate}</li>
+                        <li class="list-group-item">Tel :     {this.state.patient.telecom[0].value}</li>
+                        <li class="list-group-item">Mail :    {this.state.patient.telecom[1].value}</li>
+                        <li class="list-group-item">Adresse : {this.state.patient.address[0].line[1] + ", " + this.state.patient.address[0].line[0] + " " + this.state.patient.address[0].city}</li>
                     </ul>
                 </div>            
             );
