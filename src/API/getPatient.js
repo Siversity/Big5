2cd3477a55addd36d3732edc383dc3f581e63c0c
Victor <victor.maintenant@etud.univ-jfc.fr>
2022-09-15 14:45:49 +0200

    affichage info

--- src/API/getPatient.js
+++ src/API/getPatient.js
@@ -1,10 +1,11 @@
 import { url } from "./Constantes";
 import { idPatient } from "./Constantes";
+import axios from "axios";
 
 export function getPatient() {
     return axios({
         method: 'GET',
-        url: url + "/api/patient/"+idPatient
+        url: url + "api/patient/" + idPatient
     }).then(response => {
        return response.data;
     });
