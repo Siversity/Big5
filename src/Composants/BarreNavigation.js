b8bf60c268f3c898d789987b36e4e8f213255cc2
Thibault <thibault.teisseire@etud.univ-jfc.fr>
2022-09-15 10:52:04 +0200

    Modification package-lock.json

--- src/Composants/BarreNavigation.js
+++ src/Composants/BarreNavigation.js
@@ -1,9 +1,16 @@
-function BarreNavigation() {
-  return (
-    <div>
-        ICI LA NAVBAR
-    </div>
-  );
-}
+import React, {useState, useEffect} from 'react'
+import "./BarreNavigation.css"
 
-export default BarreNavigation;
+
+const Navbar = () => (
+    <nav className="navbar">
+        {/* Permet de revenir à la page d'accueil...à finaliser avec le href */}
+        <btn className="btn" href="PagePrincipale.js">
+            <h1>Big Five : bio results </h1>
+        </btn>
+        <ul className="navbar--link">
+            <btn className="btn">Mon profil</btn>
+        </ul>
+    </nav>
+)
+export default Navbar
