dfd2aa7c358a9b35fe295b29520b063f3c8837f2
Victor <victor.maintenant@etud.univ-jfc.fr>
2022-09-18 11:37:46 +0200

    récupération des rdv

--- src/Webpages/ListeRDV/PageListeRDV.js
+++ src/Webpages/ListeRDV/PageListeRDV.js
@@ -1,14 +1,42 @@
-// Impot React
-import React from 'react';
+import React from "react";
+import {getListeRDV} from "./../../API/getListeRDV";
 
-function PageListeRDV() {
+class PageListeRDV extends React.Component {
 
+    constructor (props){
+        super(props);
+        this.state = {
+            rdvs : null
+        };
+    }
+    
+    async componentDidMount(){
+        let r = await getListeRDV();
+        this.setState({
+            rdvs: r
+        });
+    }
 
-    return(
-        <div class="">ICI LA LISTE DES RDV
-
-        </div>
-    );
+    render(){
+        if(this.state.rdvs !== null){
+            const current = new Date(); 
+            const date = `${current.getFullYear()}-${(current.getMonth()+1).toString().padStart(2,"0")}-${current.getDate()}T00:00:00Z`;
+            return(
+                    <ul class="list-group">
+                        {this.state.rdvs.forEach(rdv =>{
+                            if(rdv.start >= date){
+                                console.log(rdv.start);
+                                <li class="list-group-item">{rdv.start}</li>
+                            }
+                        })}
+                    </ul>
+            );
+        }
+        else{
+            return(<div></div>)
+        }
+    }
+    
 }
 
 export default PageListeRDV;
